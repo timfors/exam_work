243bffaab562a6ff089bbc1815673f86072e6a38
timfors <tkhabibullin2014@litsey2.ru>
2019-07-29 15:42:53 +0300

    finish

--- exam_work/settings.py
+++ exam_work/settings.py
@@ -86,9 +86,6 @@ DATABASES = {
         'PORT': "5432"
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
